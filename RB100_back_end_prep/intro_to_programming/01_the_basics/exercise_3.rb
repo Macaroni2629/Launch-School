54cd88cbeb55de1c4d809a60bf74137dd1e64d6a
Rona Hsu <RonaHsu@gmail.com>
2020-05-24 20:40:36 +0000

    more changes

---
+++ RB100_back_end_prep/intro_to_programming/01_the_basics/exercise_3.rb
@@ -0,0 +1,11 @@
+movies = { :Jurassic_Park => 1975,
+            :Carrie => 2004,
+            :Harry_Potter => 2013,
+            :Band_of_Brothers => 2001,
+            :Fantastic_Four => 1981 }
+
+puts movies[:Jurassic_Park]
+puts movies[:Carrie]
+puts movies[:Harry_Potter]
+puts movies[:Band_of_Brothers]
+puts movies[:Fantastic_Four]
