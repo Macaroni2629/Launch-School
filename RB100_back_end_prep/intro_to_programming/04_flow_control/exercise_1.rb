5d78861989e15b84d487e12350b558825841462e
Rona Hsu <RonaHsu@gmail.com>
2020-05-25 12:52:03 +0000

    added exercise 1

---
+++ RB100_back_end_prep/intro_to_programming/04_flow_control/exercise_1.rb
@@ -0,0 +1,5 @@
+# (32 * 4) >= 129 returns false because 128 is not greater than or equal to 239
+# false != !true returns false because false does equal false
+# true == 4 returns false because true is not equal to 4
+# false == (847 == '874') returns true because false equals false
+# (!true || (!(100 / 5) == 20) || ((328 / 4) == 82)) || false  answer is true
