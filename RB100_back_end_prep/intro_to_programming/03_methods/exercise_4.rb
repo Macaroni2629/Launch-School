944218383b98c679154a7ead9b7f9726dcb785b8
Rona Hsu <RonaHsu@gmail.com>
2020-05-25 12:11:00 +0000

    Added exercise 4

---
+++ RB100_back_end_prep/intro_to_programming/03_methods/exercise_4.rb
@@ -0,0 +1,14 @@
+def scream(words)
+    words = words + "!!!!"
+    return
+    puts words
+end
+
+scream("Yippeee")
+
+=begin 
+
+This will return nothing because of the reserve word "return."  Return means
+it will immediately exit the method.
+
+=end
