6493e77d30623888e840582f60cb7eb447174adf
Rona Hsu <RonaHsu@gmail.com>
2020-05-25 20:55:00 +0000

    added exercise 2

---
+++ RB100_back_end_prep/intro_to_programming/06_arrays/exercise_02.rb
@@ -0,0 +1,13 @@
+# arr = ["b", "a"]
+# arr = arr.product(Array(1..3))
+# arr.first.delete(arr.first.last)
+
+# returns 1 which is the last element of the first element of arr
+ # arr =  [["b"], ["b", 2], ["b", 3], ["a", 1], ["a", 2], ["a", 3]]
+ 
+ #arr = ["b", "a"]
+ #arr = arr.product([Array(1..3)])
+ #arr.first.delete(arr.first.last)
+ 
+ #returns [1, 2, 3] because the array is the last element of the first element
+#arr = [["b"], ["a", [1, 2, 3]]]
