1a2a4fdf35e659603e30c962512328cb67a41f2d
Rona Hsu <RonaHsu@gmail.com>
2020-05-26 01:13:09 +0000

    added exercise 6 and 7

---
+++ RB100_back_end_prep/intro_to_programming/06_arrays/exercise_06.rb
@@ -0,0 +1,4 @@
+# names = ['bob', 'joe', 'susan', 'margaret']
+# names['margaret'] = 'jody'
+
+# code doesn't run because names is an array.  Arrays are indexed with integers, not strings.
---
+++ RB100_back_end_prep/intro_to_programming/06_arrays/exercise_07.rb
@@ -0,0 +1,6 @@
+array = [9, 2, 0, 1, 5]
+
+array.each_with_index do |number, index|
+    puts "#{index + 1}. #{number}"
+end 
+
