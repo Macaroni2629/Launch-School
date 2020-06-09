aceff70b38a2362f488859e624eae116981a86ad
Rona Hsu <RonaHsu@gmail.com>
2020-06-09 11:08:49 +0000

    added second exercise

---
+++ RB100_back_end_prep/ruby_exercises/reading_documentation/while_loop.rb
@@ -0,0 +1,7 @@
+puts "In order to find the while loop, please do the following:
+
+1) Select the correct core API version for the version of Ruby you are using.
+
+2) Select 'control expressions.'
+
+3) Look through for 'while loop.'"
