beec344741aa60e1637b52c63eed4a8bd78cb556
Rona Hsu <RonaHsu@gmail.com>
2020-06-11 06:34:16 -0500

    added exercise whats my value part 2

--- RB100_back_end_prep/ruby_exercises/variable_scope/whats_my_value_part2.rb
+++ RB100_back_end_prep/ruby_exercises/variable_scope/whats_my_value_part2.rb
@@ -7,3 +7,6 @@ end
 my_value(a)
 
 puts a
+
+# the answer is 7 because method definitions are self contained with respect to local variables.
+# Local variables outside the method definition are not visible inside the method definition.
