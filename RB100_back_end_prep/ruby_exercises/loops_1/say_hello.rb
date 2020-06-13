d7290aa48c953f93a0f539586e93bdbc10036da3
Rona Hsu <RonaHsu@gmail.com>
2020-06-13 15:58:15 -0500

    added say hello exercise

---
+++ RB100_back_end_prep/ruby_exercises/loops_1/say_hello.rb
@@ -0,0 +1,12 @@
+say_hello = true
+count = 0
+
+while say_hello
+    puts 'Hello!'
+    count += 1
+    say_hello = false if count == 5
+end
+
+
+# You need a count to increment and count the number of iterations.
+# You need to set say_hello = false if/when count == 5 in order to break the loop.
