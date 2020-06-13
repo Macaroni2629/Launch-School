fd5d56f3983990567ab832509f33db5b67c24588
Rona Hsu <RonaHsu@gmail.com>
2020-06-13 10:41:06 -0500

    added runaway loop exercise

---
+++ RB100_back_end_prep/ruby_exercises/loops_1/runaway_loop.rb
@@ -0,0 +1,7 @@
+loop do
+    puts 'just keep printing...'
+    break
+end
+
+#one must place a break after the puts statement so that the statement gets printed.
+#one must place a break inside a loop so that the loop does not become infinite.
