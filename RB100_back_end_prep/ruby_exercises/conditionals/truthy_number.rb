aef56740c27595e24ff78997912c8862127f7064
Rona Hsu <RonaHsu@gmail.com>
2020-06-18 06:13:20 -0500

    added truthy number exercise

---
+++ RB100_back_end_prep/ruby_exercises/conditionals/truthy_number.rb
@@ -0,0 +1,10 @@
+number = 7
+
+if number
+  puts "My favorite number is #{number}."
+else
+  puts "I don't have a favorite number."
+end
+
+# The key here is that every expression evaluates to true when used in a condition.
+# ( Except for false and nil. )
