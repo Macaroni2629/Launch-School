91e36e61fb4f6a2b5e81ee9d94ca90bb4c6a824e
Rona Hsu <RonaHsu@gmail.com>
2020-06-19 05:35:14 -0500

    added spotlight part 2 exercise

---
+++ RB100_back_end_prep/ruby_exercises/conditionals/spotlight_part2.rb
@@ -0,0 +1,12 @@
+spotlight = ['green', 'yellow', 'red'].sample
+
+if stoplight == 'green'
+  puts 'Go!'
+elsif stoplight == 'yellow'
+  puts 'Slow down!'
+else
+  puts 'Stop!'
+end
+
+# As compared to the case statement, the case statement is best suited
+# for comparing multiple values to the same case.
