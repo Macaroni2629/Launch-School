c1ab6b81464eb1df318c48c7ce2c103f330cd13b
Rona Hsu <RonaHsu@gmail.com>
2020-06-19 05:26:27 -0500

    added spotlight part 1 exercise

---
+++ RB100_back_end_prep/ruby_exercises/conditionals/spotlight_part1.rb
@@ -0,0 +1,12 @@
+stoplight = ['green', 'yellow', 'red'].sample
+
+case stoplight
+when 'green'
+  puts 'Go!'
+when 'yellow'
+  puts 'Slow down!'
+else
+  puts 'Stop!'
+end
+
+# Note that 'red' is not explicitly stated, but it's implied due to process of elimination.
