5721e457f043118b2b721d4413d39e7811d4a2d1
Rona Hsu <ronahsu@gmail.com>
2020-07-18 16:07:44 -0500

    added exercise 4

--- 101_programming_foundations/RB101_109_small_problems/01_easy/04_how_many.rb
+++ 101_programming_foundations/RB101_109_small_problems/01_easy/04_how_many.rb
@@ -1,1 +1,31 @@
-asdf
+# How Many?
+# Write a method that counts the number of occurrences of each element in a given array.
+
+vehicles = [
+  'car', 'car', 'truck', 'car', 'SUV', 'truck',
+  'motorcycle', 'motorcycle', 'car', 'truck'
+]
+
+# count_occurrences(vehicles)
+# The words in the array are case-sensitive: 'suv' != 'SUV'. ` Once counted, print each element alongside the number of occurrences.
+
+# Expected output:
+
+# car => 4
+# truck => 3
+# SUV => 1
+# motorcycle => 2
+
+def count_occurrences(array)
+  occurrences = {}
+
+  array.uniq.each do |element|
+    occurrences[element] = array.count(element)
+  end
+  
+  occurrences.each do |element, count|
+    puts "#{element} => #{count}"
+  end
+end
+
+p count_occurrences(vehicles)
