78099b3a17599d7207f4dd7a624e418c70736f04
Rona Hsu <ronahsu@gmail.com>
2020-07-23 09:38:47 -0500

    added calculator bonus features

---
+++ 101_programming_foundations/lesson_2/03_calculator_bonus_features.rb
@@ -0,0 +1,122 @@
+require 'yaml'
+MESSAGES = YAML.load_file('02_calculator_messages.yml')
+LANGUAGE = 'en'
+
+def messages(message, lang='en')
+  MESSAGES[lang][message]
+end
+
+def prompt(key, variable=nil)
+  message = messages(key, LANGUAGE)
+  if variable
+    message_with_variable = message % { :variable => variable }
+    Kernel.puts ("=> #{message_with_variable}")
+  else
+    Kernel.puts("=> #{message}")
+  end
+end
+
+def valid_number?(num)
+  integer?(num) || float?(num)
+end
+
+def integer?(input)
+  input.match?(/^\d+$/)
+end
+
+def float?(input)
+  input.match?(/^\d*\.?\d*$/)
+end
+
+def operation_to_message(op, lang=en)
+  word = case op
+  when '1'
+    MESSAGES[lang]['adding']
+  when '2'
+    MESSAGES[lang]['subtracting']
+  when '3'
+    MESSAGES[lang]['multiplying']
+  when '4'
+    MESSAGES[lang]['dividing']
+  end
+
+  word
+end
+
+prompt('welcome')
+
+name = ''
+loop do
+  name = Kernel.gets().chomp()
+
+  if name.empty?()
+    prompt('valid_name')
+  else
+    break
+  end
+end
+
+prompt('welcome_name', name)
+
+loop do # main loop
+  
+  number1 = ''
+  loop do
+    prompt('number_1')
+    number1 = Kernel.gets().chomp()
+
+    if valid_number?(number1)
+      break
+    else
+      prompt('valid_number')
+    end
+  end
+
+  number2 = ''
+  loop do
+    prompt('number_2')
+    number2 = Kernel.gets().chomp()
+
+    if valid_number?(number2)
+      break
+    else
+      prompt('valid_number')
+    end
+  end
+
+  promp('operation_question')
+
+  operator = ''
+
+  loop do
+    operator = Kernel.gets().chomp()
+
+    if %w(1 2 3 4).include?(operator)
+      break
+    else
+      prompt('valid_operator')
+    end
+  end
+
+  prompt('operation_statement', operation_to_message(operator, LANGUAGE))
+
+  result =
+    case operator
+    when '1'
+      number1.to_i() + number2.to_i()
+    when '2'
+      number1.to_i() + number2.to_i()
+    when '3'
+      number1.to_i() * number2.to_i()
+    when '4'
+      number1.to_f() / number2.to_f()
+    end
+
+  prompt('result', result)
+
+  prompt('restart')
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase.start_with?('y')
+end
+
+prompt('thank_you')
