224e4bf1aa021c362f0c213e4827825eae8ebb92
Rona <RonaHsu@gmail.com>
2020-08-19 06:34:43 -0500

    added my version of refactored calculator

---
+++ 101_programming_foundations/lesson_2/02_my_refactoring_calculator.rb
@@ -0,0 +1,76 @@
+=begin
+Refactoring Calculator Problem
+
+Algorithm
+Ask user for one integer
+  Validate
+    Use a loop to validate
+
+Ask user for second integer
+  Validate
+    Use a loop to validate
+
+Ask user for operation
+  Validate operation
+  
+Do Calculation
+
+Ask if they want to do another calculation.
+
+
+=end
+
+def prompt(msg)
+  puts("=>#{msg}")
+end
+
+def get_user_input
+  gets.chomp
+end
+
+def valid_number?(number)
+  number.to_i.to_s == number
+end
+
+def valid_operation?(operation)
+  %w[1 2 3 4].include?(operation)
+end
+
+def get_num_and_validate
+  loop do
+    prompt("Please input a number.")
+    number = get_user_input
+    return number if valid_number?(number)
+    prompt("Invalid input.  Please enter an integer.")
+  end
+end
+
+def get_operation
+  prompt("Please enter an operation.  1 for addition, 2 for subtraction, 3 for multiplication, 4 for division.")
+  gets.chomp
+end
+
+loop do
+  number1 = get_num_and_validate
+  number2 = get_num_and_validate
+  operation = get_operation
+
+  result = case operation
+           when '1'
+            number1.to_i + number2.to_i
+           when '2'
+            number1.to_i - number2.to_i
+           when '3'
+            number1.to_i * number2.to_i
+           when '4'
+            number1.to_f / number2.to_f
+  end
+
+  prompt("The result is #{result}.")
+  prompt("Do you want to do another calculation.  Type 'y' for yes.  Anything else will leave the calculator.")
+
+  answer = gets.chomp
+  break unless answer.downcase == 'y'
+end
+      
+prompt("Thank you for using the calculator.  Goodbye!")
