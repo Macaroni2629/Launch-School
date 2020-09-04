e61a92bea99c50cfc7eb5bd5ad777aee1c36797e
Rona <RonaHsu@gmail.com>
2020-09-04 05:08:43 -0500

    added question 1

---
+++ 101_programming_foundations/lesson_3/easy_2/question_1.rb
@@ -0,0 +1,10 @@
+# In this hash of people and their age,
+
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
+see if "Spot" is present.
+
+ages.key?("Spot")
+
+ages.include?("Spot")
+
+ages.member?("Spot")
