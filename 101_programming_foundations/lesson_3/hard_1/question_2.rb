122c82232972354e03e993fd17198a3280526138
Rona <RonaHsu@gmail.com>
2020-09-12 05:36:26 -0500

    added question 2

---
+++ 101_programming_foundations/lesson_3/hard_1/question_2.rb
@@ -0,0 +1,15 @@
+=begin
+  
+What is the result of the last line in the code below?
+
+greetings = { a: 'hi' }
+informal_greeting = greetings[:a]
+informal_greeting << ' there'
+
+puts informal_greeting  #  => "hi there"
+puts greetings
+
+
+# greetings will return {a: 'hi there'}
+
+=end
