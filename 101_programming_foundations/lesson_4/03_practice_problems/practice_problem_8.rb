ef036ef384e45b08491b7229257d4644f02949d4
Rona <RonaHsu@gmail.com>
2020-09-23 17:39:19 -0500

    added practice problem 8

---
+++ 101_programming_foundations/lesson_4/03_practice_problems/practice_problem_8.rb
@@ -0,0 +1,6 @@
+arr = [1, 2, 3, 4, 5]
+arr.take(2)
+
+# take returns first n elements of an array.  It is not a destructive method.
+
+# We can find out by evaluating the original array and seeing what it returns.
