d0c5f13fbb8b60071433320a1ba113aafb0b2688
Rona Hsu <ronahsu@gmail.com>
2020-10-03 09:50:24 -0500

    added exercise 13

---
+++ 101_programming_foundations/lesson_5/02_practice_problems/exercise_13.rb
@@ -0,0 +1,13 @@
+# Given the following data structure, return a new array containing the same sub-arrays as the original but ordered logically by only taking into consideration the odd numbers they contain.
+
+arr = [[1, 6, 7], [1, 4, 9], [1, 8, 3]]
+# The sorted array should look like this:
+
+
+# => [[1, 8, 3], [1, 6, 7], [1, 4, 9]]
+
+p(arr.sort_by do |subarray|
+  subarray.select do |number|
+    number.odd?
+  end
+end)
