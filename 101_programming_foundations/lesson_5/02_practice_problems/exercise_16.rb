32620c694d16b13578afa5471016d48e8c3d8882
Rona <RonaHsu@gmail.com>
2020-10-03 17:46:22 -0500

    added exercise 16

--- 101_programming_foundations/lesson_5/02_practice_problems/exercise_16.rb
+++ 101_programming_foundations/lesson_5/02_practice_problems/exercise_16.rb
@@ -1,13 +1,24 @@
-# Given this data structure write some code to return an array which contains only the hashes where all the integers are even.
+# A UUID is a type of identifier often used as a way to uniquely identify items...which may not all be created by the same system. That is, without any form of synchronization, two or more separate computer systems can create new items and label them with a UUID with no significant chance of stepping on each other's toes.
 
+# It accomplishes this feat through massive randomization. The number of possible UUID values is approximately 3.4 X 10E38.
 
-arr = [{a: [1, 2, 3]}, {b: [2, 4, 6], c: [3, 6], d: [4]}, {e: [8], f: [6, 10]}]
+# Each UUID consists of 32 hexadecimal characters, and is typically broken into 5 sections like this 8-4-4-4-12 and represented as a string.
 
+# It looks like this: "f65c57f6-a6aa-17a8-faa1-a67f2dc9fa91"
 
-p (arr.select do |hash|
-  hash.all? do |_, value|
-    value.all? do |number|
-      number.even?
-    end
+# Write a method that returns one UUID when called with no parameters.
+
+def generate_UUID
+  characters = []
+  (0..9).each { |digit| characters << digit.to_s }
+  ('a'..'f').each { |digit| characters << digit }
+
+  uuid = ""
+  sections = [8, 4, 4, 4, 12]
+  sections.each_with_index do |section, index|
+    section.times { uuid += characters.sample }
+    uuid += '-' unless index >= sections.size - 1
   end
-end)
+
+  uuid
+end
