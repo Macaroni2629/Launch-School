4e0cfdc8a4f644ba2b138c3ec96089b27cdd1a9e
Rona <RonaHsu@gmail.com>
2020-10-06 06:37:34 -0500

    added tic tac toe bonus

--- 101_programming_foundations/lesson_6/tic_tac_toe_bonus.rb
+++ 101_programming_foundations/lesson_6/tic_tac_toe_bonus.rb
@@ -1,17 +1,78 @@
-def display_board
+require 'pry'
+
+INITIAL_MARKER = ' '
+PLAYER_MARKER = 'X'
+COMPUTER_MARKER = 'O'
+
+def prompt(msg)
+  puts "=> #{msg}"
+end
+
+def display_board(brd)
+  system 'clear'
   puts ""
   puts "     |     |"
-  puts "     |     |"
-  puts "     |     |"
-  puts "-----+-----|-----"
-  puts "     |     |"
-  puts "     |     |"
+  puts "   #{brd[1]} |  #{brd[2]} |   #{brd[3]}"
   puts "     |     |"
   puts "-----+-----|-----"
   puts "     |     |"
+  puts "   #{brd[4]}|  #{brd[5]} |    #{brd[6]}"
   puts "     |     |"
+  puts "-----+-----|-----"
+  puts "     |     |"
+  puts "   #{brd[7]}   |  #{brd[8]} |  #{brd[9]}"
   puts "     |     |"
   puts ""
 end
 
-display_board
+def initialize_board
+  new_board = {}
+  (1..9).each {|num| new_board[num] = INITIAL_MARKER}
+  new_board
+end
+
+def empty_squares(brd)
+  brd.keys.select{|num| brd[num] == INITIAL_MARKER}
+end
+
+def player_places_piece!(board)
+  square = ''
+  loop do
+    prompt "Choose a square #{empty_squares(board).join( ', ')}:"
+    square = gets.chomp.to_i
+    break if empty_squares(board).include?(square)
+    prompt "Sorry, that's not a valid choice."
+  end
+    board[square] = PLAYER_MARKER
+end
+
+def computer_places_piece!(board)
+  square = empty_squares(board).sample
+  board[square] = COMPUTER_MARKER
+end
+
+def board_full?(board)
+  empty_squares(board).empty?
+end
+
+def someone_won?(board)
+  !!detect_winner(board)
+end
+
+board = initialize_board
+display_board(board)
+
+loop do
+  player_places_piece!(board)
+  computer_places_piece!(board)
+  display_board(board)
+  break if someone_won? || board_full?(board)
+end
+
+if someone_won?(board)
+  prompt "#{detect_winner(board)} won!"
+else
+  prompt "It's a tie!"
+end
+
+display_board(board)
