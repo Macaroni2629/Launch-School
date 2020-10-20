5e7e91e36dee2af27215e9affa139779a4c20542
Rona <RonaHsu@gmail.com>
2020-10-20 18:33:09 -0500

    added greeting a user

---
+++ 101_programming_foundations/RB101_109_small_problems/02_easy/05_greeting_a_user.rb
@@ -0,0 +1,26 @@
+# Greeting a user
+# Write a program that will ask for user's name. The program will then greet the user. If the user writes "name!" then the computer yells back to the user.
+
+# Examples
+
+# What is your name? Bob
+# Hello Bob.
+
+# What is your name? Bob!
+# HELLO BOB. WHY ARE WE SCREAMING?
+
+def greeting
+  puts "What's your name?"
+  name = gets.chomp
+  
+  if name.include?("!")
+    name = name.upcase.delete("!")
+    puts "HELLO #{name}.  WHY ARE WE SCREAMING?"
+  else
+    puts "Hello #{name}."
+  end
+    
+  
+end
+
+greeting
