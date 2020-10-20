3b6b5b57887ea8ac38606dfa2ef46ab80b3a2235
Rona <RonaHsu@gmail.com>
2020-10-20 18:53:15 -0500

    added even numbers

---
+++ 101_programming_foundations/RB101_109_small_problems/02_easy/07_even_numbers.rb
@@ -0,0 +1,8 @@
+# Even Numbers
+# Print the even numbers from 1 to 99, inclusive. All numbers should be printed on separate lines.
+  
+value = 0
+while value < 100
+  puts value if value.even?
+  value += 1
+end
