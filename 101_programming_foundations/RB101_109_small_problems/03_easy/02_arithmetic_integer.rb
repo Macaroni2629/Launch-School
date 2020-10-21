f14d8edb192af15acb21d5bd91bc0d4edefc1537
Rona <RonaHsu@gmail.com>
2020-10-21 17:43:20 -0500

    added arithmetic integer

---
+++ 101_programming_foundations/RB101_109_small_problems/03_easy/02_arithmetic_integer.rb
@@ -0,0 +1,36 @@
+# Arithmetic Integer
+# Write a program that prompts the user for two positive integers, and then prints the results of the following operations on those two numbers: addition, subtraction, product, quotient, remainder, and power. Do not worry about validating the input.
+
+# Example
+
+# ==> Enter the first number:
+# 23
+# ==> Enter the second number:
+# 17
+# ==> 23 + 17 = 40
+# ==> 23 - 17 = 6
+# ==> 23 * 17 = 391
+# ==> 23 / 17 = 1
+# ==> 23 % 17 = 6
+# ==> 23 ** 17 = 141050039560662968926103
+
+def prompt(msg)
+  puts "==> #{msg}"
+end
+
+def arith_int
+  prompt "Enter the first number:"
+  number1 = gets.chomp.to_i
+  prompt "Enter the second number:"
+  number2 = gets.chomp.to_i
+  
+  prompt "#{number1} + #{number2} = #{number1 + number2}"
+  prompt "#{number1} - #{number2} = #{number1 - number2}"
+  prompt "#{number1} * #{number2} = #{number1 * number2}"
+  prompt "#{number1} / #{number2} = #{number1 / number2}"
+  prompt "#{number1} % #{number2} = #{number1 % number2}"
+  prompt "#{number1} ** #{number2} = #{number1 ** number2}"
+  
+end
+
+arith_int
