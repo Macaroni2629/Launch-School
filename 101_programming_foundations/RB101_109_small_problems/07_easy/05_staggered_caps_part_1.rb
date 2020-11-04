7aa1c4dfca49dcb26d75ca789538f77d89e2832b
Rona <RonaHsu@gmail.com>
2020-11-04 05:53:27 -0600

    added staggered caps

---
+++ 101_programming_foundations/RB101_109_small_problems/07_easy/05_staggered_caps_part_1.rb
@@ -0,0 +1,33 @@
+# Staggered Caps (Part 1)
+# Write a method that takes a String as an argument, and returns a new String that contains the original value using a staggered capitalization scheme in which every other character is capitalized, and the remaining characters are lowercase. Characters that are not letters should not be changed, but count as characters when switching between upper and lowercase.
+
+# Example:
+
+def staggered_case(string)
+  string.downcase.each_char.map.with_index do |char, index|
+    if index.even?
+      char.upcase
+    else
+      char
+    end
+  end.join
+end
+
+def staggered_case(string)
+  result = ''
+  need_upper = true
+  string.chars.each do |char|
+    if need_upper
+      result += char.upcase
+    else
+      result += char.downcase
+    end
+    need_upper = !need_upper
+  end
+  result
+end
+
+
+p staggered_case('I Love Launch School!') == 'I LoVe lAuNcH ScHoOl!'
+p staggered_case('ALL_CAPS') == 'AlL_CaPs'
+p staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 NuMbErS'
