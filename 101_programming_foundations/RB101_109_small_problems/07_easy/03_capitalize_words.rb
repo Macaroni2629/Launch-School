05efc6caee9841d0aa69221ea405da5bb1b77a2c
Rona <RonaHsu@gmail.com>
2020-11-03 20:37:08 -0600

    added capitalize words

---
+++ 101_programming_foundations/RB101_109_small_problems/07_easy/03_capitalize_words.rb
@@ -0,0 +1,31 @@
+# Capitalize Words
+# Write a method that takes a single String argument and returns a new string that contains the original value of the argument with the first character of every word capitalized and all other letters lowercase.
+
+# You may assume that words are any sequence of non-blank characters.
+
+# Examples
+
+def word_cap(string)
+  array_of_words = string.downcase.split
+  
+  array_of_words.map do |word|
+    word[0] = word[0].upcase
+    word
+  end.join(" ")
+end
+
+def word_cap(words)
+  words_array = words.split.map do |word|
+    word.capitalize
+  end
+  words_array.join(' ')
+end
+
+def word_cap(words)
+  words.split.map(&:capitalize).join(' ')
+end
+
+
+p word_cap('four score and seven') == 'Four Score And Seven'
+p word_cap('the javaScript language') == 'The Javascript Language'
+p word_cap('this is a "quoted" word') == 'This Is A "quoted" Word'
