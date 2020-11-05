23ecb7859d3fa6c88906b64c776a5aae82c3bedb
Rona <RonaHsu@gmail.com>
2020-11-05 05:55:36 -0600

    added end is near

---
+++ 101_programming_foundations/RB101_109_small_problems/07_easy/10_end_is_near.rb
@@ -0,0 +1,19 @@
+# The End Is Near But Not Here
+# Write a method that returns the next to last word in the String passed to it as an argument.
+
+# Words are any sequence of non-blank characters.
+
+# You may assume that the input String will always contain at least two words.
+
+# Examples:
+
+def penultimate(string)
+  array_of_words = string.split
+  
+  array_of_words[-2]
+  
+end
+
+
+p penultimate('last word') == 'last'
+p penultimate('Launch School is great!') == 'is'
