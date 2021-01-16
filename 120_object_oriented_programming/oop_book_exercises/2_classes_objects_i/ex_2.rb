da595ed6b31eb895c4fe77b0ba91ff98cfb3018a
Rona Hsu <ronahsu@gmail.com>
2021-01-16 09:12:35 -0600

    added ex2

---
+++ 120_object_oriented_programming/oop_book_exercises/2_classes_objects_i/ex_2.rb
@@ -0,0 +1,42 @@
+# Add an accessor method to your
+#  MyCar class to change and view
+#   the color of your car. Then add
+#    an accessor method that allows 
+#    you to view, 
+# but not modify, the year of your car.
+
+class MyCar
+
+  attr_accessor :color
+  attr_reader :year
+
+  def initialize(year, model, color)
+    @year = year
+    @model = model
+    @color = color
+    @current_speed = 0
+  end
+
+  def speed_up(number)
+    @current_speed += number
+    puts "You push the gas and accelerate #{number} mph."
+  end
+
+  def brake(number)
+    @current_speed -= number
+    puts "You push the brake and decelerate #{number} mph."
+  end
+
+  def current_speed
+    puts "You are now going #{@current_speed} mph."
+  end
+
+  def shut_down
+    @current_speed = 0
+    puts "Let's park this bad boy!"
+  end
+end
+
+lumina.color = 'black'
+puts lumina.color
+puts lumina.year
