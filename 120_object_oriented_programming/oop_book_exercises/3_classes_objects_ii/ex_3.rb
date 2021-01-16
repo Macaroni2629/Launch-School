7782e52610f44535aef3495ba36d6731961008c8
Rona Hsu <ronahsu@gmail.com>
2021-01-16 14:43:03 -0600

    added ex3

--- 120_object_oriented_programming/oop_book_exercises/3_classes_objects_ii/ex_3.rb
+++ 120_object_oriented_programming/oop_book_exercises/3_classes_objects_ii/ex_3.rb
@@ -24,14 +24,5 @@ bob.name = "Bob"
 #  or attr_writer if we don't intend to use the getter 
 #  functionality.
 
-class Person
-  attr_accessor :name
-  # attr_writer :name ## => This also works but doesn't allow getter access
-  def initialize(name)
-    @name = name
-  end
-end
 
-bob = Person.new("Steve")
-bob.name = "Bob"
 
