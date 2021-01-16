b8117ec37d8c7ac2744f0442f758f6c0580ce8d6
Rona <RonaHsu@gmail.com>
2021-01-16 06:51:10 -0600

    added the object model

---
+++ 120_object_oriented_programming/oop_book_exercises/1_the_object_model/ex_1.rb
@@ -0,0 +1,13 @@
+# How do we create an object in Ruby? 
+#   Give an example of the creation of an object.
+
+class Cat
+end
+
+socks = Cat.new
+
+=begin
+  We create an object by defining a class using the 'class' keyword and the reserved word 'end'.
+  We then instantiate the object and named it 'socks' in this case. 
+We create an 'instance' or also known as an object by calling the `new` class method.
+=end
