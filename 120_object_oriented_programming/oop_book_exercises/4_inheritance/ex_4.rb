ab7b41cfa2ecbcf13cab63801cfd421f5328c955
Rona Hsu <ronahsu@gmail.com>
2021-01-16 15:19:28 -0600

    added ex4

---
+++ 120_object_oriented_programming/oop_book_exercises/4_inheritance/ex_4.rb
@@ -0,0 +1,8 @@
+#Print to the screen your method lookup for the classes that 
+#you have created.
+
+# code omitted for brevity...
+
+puts MyCar.ancestors
+puts MyTruck.ancestors
+puts Vehicle.ancestors
