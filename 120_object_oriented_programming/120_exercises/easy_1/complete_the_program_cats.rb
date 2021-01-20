0a8e03d825fd87c215214549761f8dd7c6f61da8
Rona <RonaHsu@gmail.com>
2021-01-20 06:34:16 -0600

    added complete the program

---
+++ 120_object_oriented_programming/120_exercises/easy_1/complete_the_program_cats.rb
@@ -0,0 +1,34 @@
+# Complete the Program - Cats!
+# Consider the following program.
+
+
+class Pet
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+end
+
+class Cat < Pet
+end
+
+pudding = Cat.new('Pudding', 7, 'black and white')
+butterscotch = Cat.new('Butterscotch', 10, 'tan and white')
+puts pudding, butterscotch
+
+
+class Cat < Pet
+  def initialize(name, age, colors)
+    super(name, age)
+    @colors = colors
+  end
+
+  def to_s
+    "My cat #{@name} is #{@age} years old and has #{@colors} fur."
+  end
+end
+# Update this code so that when you run it, you see the following output:
+
+
+# My cat Pudding is 7 years old and has black and white fur.
+# My cat Butterscotch is 10 years old and has tan and white fur.
