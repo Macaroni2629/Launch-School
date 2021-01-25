789b16311f4c22e91200cfcfa6852e48781e00a6
Rona <RonaHsu@gmail.com>
2021-01-25 12:01:56 -0600

    added pet shelter 2

--- 120_object_oriented_programming/120_exercises/easy_2/pet_shelter_2.rb
+++ 120_object_oriented_programming/120_exercises/easy_2/pet_shelter_2.rb
@@ -70,3 +70,49 @@ shelter.adopt(bholmes, chester)
 shelter.print_adoptions
 puts "#{phanson.name} has #{phanson.number_of_pets} adopted pets."
 puts "#{bholmes.name} has #{bholmes.number_of_pets} adopted pets."
+
+class Pet
+#   attr_reader :animal_type, :name
+#   def initialize(animal_type, name)
+#     @animal_type = animal_type
+#     @name = name
+#   end
+# end
+
+# class Owner
+#   attr_reader :name
+#   attr_accessor :number_of_pets
+  
+#   def initialize(name)
+#     @name = name
+#     @number_of_pets = 0
+#   end
+# end
+
+# class Shelter
+#   #attr_reader :adoption_record # self.adoption_record (getter) ??????
+#   def initialize
+#     @adoption_record = {} # {owner.name: [animal1, animal2]}
+#   end 
+  
+#   def adopt(owner, animal)
+#     # now we have access to one owner and one animal (objects)
+#     if @adoption_record[owner] == nil
+#       @adoption_record[owner] = [animal]
+#     else
+#       @adoption_record[owner] << animal
+#     end
+#     owner.number_of_pets += 1
+#   end
+  
+#   def print_adoptions
+#     # @adoption_record => { phanson => [butterscotch, pudding, darwin], ...}
+#     @adoption_record.each do | owner, array_of_pets |
+#       puts "#{owner.name} has adopted the following pets:"
+#       array_of_pets.each do |pet|
+#         puts "a #{pet.animal_type} named #{pet.name}"
+#       end
+#       puts ""
+#     end
+#   end
+# end
