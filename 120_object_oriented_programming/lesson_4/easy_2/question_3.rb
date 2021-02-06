505424d4aa071f879a639f38ecb8a60feccc63f9
Rona <RonaHsu@gmail.com>
2021-02-06 16:38:17 -0600

    added question 3

---
+++ 120_object_oriented_programming/lesson_4/easy_2/question_3.rb
@@ -0,0 +1,29 @@
+# How do you find where Ruby will look for a method when that method is called? How can you find an object's ancestors?
+
+module Taste
+  def flavor(flavor)
+    puts "#{flavor}"
+  end
+end
+
+class Orange
+  include Taste
+end
+
+class HotSauce
+  include Taste
+end
+
+#What is the lookup chain for Orange and HotSauce?
+
+# To get the ancestors of a particular class you can ask the class itself and it will tell you directly if you call ancestors on it.
+
+# For example:
+
+# >> HotSauce.ancestors
+# => [HotSauce, Taste, Object, Kernel, BasicObject]
+# The list of ancestor classes is also called a lookup chain, because Ruby will look for a method starting in the first class in the chain (in this case HotSauce) and eventually lookup BasicObject if the method is found nowhere in the lookup chain.
+
+# If the method appears nowhere in the chain then Ruby will raise a NoMethodError which will tell you a matching method can not be found anywhere in the chain.
+
+# Keep in mind this is a class method and it will not work if you call this method on an instance of a class (unless of course that instance has a method called ancestors).
