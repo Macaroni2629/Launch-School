fd1fa8b7e66b5e37da27aa9e30e9750060e9e0cd
Rona <RonaHsu@gmail.com>
2021-02-06 17:14:19 -0600

    added question 5

---
+++ 120_object_oriented_programming/lesson_4/easy_2/question_5.rb
@@ -0,0 +1,11 @@
+There are a number of variables listed below. What are the different types and how do you know which is which?
+  
+excited_dog = "excited dog"
+@excited_dog = "excited dog"
+@@excited_dog = "excited dog"
+  
+Here we have the following variables:
+
+Local variable - excited_dog Instance variable - @excited_dog Class variable - @@excited_dog
+
+We can tell which is which by how the variables are prefixed. Local variables do not contain anything prefixed, while instance variables are prefixed with the @ and class variables are prefixed with @@.
