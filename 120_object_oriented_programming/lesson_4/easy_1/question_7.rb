f5b0823a2deff925a6f6b3ce7caf461f97e68cb0
Rona <RonaHsu@gmail.com>
2021-02-06 06:03:56 -0600

    added question 7

---
+++ 120_object_oriented_programming/lesson_4/easy_1/question_7.rb
@@ -0,0 +1,5 @@
+# What is the default return value of to_s when invoked on an object? Where could you go to find out if you want to be sure?
+
+# By default, the to_s method will return the name of the object's class and an encoding of the object id.
+
+# If you weren't sure of this answer you could of course refer to Launch School's Object Oriented Programming book, but you could also refer directly to the Ruby documentation, in this case, here: http://ruby-doc.org/core/Object.html#method-i-to_s.
